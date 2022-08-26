675dffc867f0e6e07971563e84d6b1561b4cd1d0
stux <hello@mstdn.social>
2022-08-26 12:04:00 +0200

    Landing page banner

--- resources/js/Pages/Landing.vue
+++ resources/js/Pages/Landing.vue
@@ -12,14 +12,67 @@ defineProps({
     <GuestLayout title="Welcome">
 
         <section class="bg-white dark:bg-gray-900">
-            <div class="gap-8 items-center pt-10 px-6 mx-auto max-w-screen-xl xl:gap-16 md:grid md:grid-cols-2 lg:px-6">
+            <div class="py-8 px-4 mx-auto max-w-screen-xl text-center lg:py-16 lg:px-12">
+                <InertiaLink href="/about"
+                    class="inline-flex justify-between items-center py-1 px-1 pr-4 mb-7 text-sm text-gray-700 bg-gray-100 rounded-full dark:bg-gray-800 dark:text-white hover:bg-gray-200 dark:hover:bg-gray-700"
+                    role="alert">
+                    <span
+                        class="text-xs bg-primary-600 rounded-full dark:text-white text-gray-800 px-4 py-1.5 mr-3">Features:</span>
+                    <span class="text-sm font-medium">What we are all about</span>
+                    <svg class="ml-2 w-5 h-5" fill="currentColor" viewBox="0 0 20 20"
+                        xmlns="http://www.w3.org/2000/svg">
+                        <path fill-rule="evenodd"
+                            d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z"
+                            clip-rule="evenodd"></path>
+                    </svg>
+                </InertiaLink>
+                <h1
+                    class="mb-4 text-4xl font-extrabold tracking-tight leading-none text-gray-900 md:text-5xl lg:text-6xl dark:text-white">
+                    A simple, clean tool for you.</h1>
+                <p class="mb-8 text-lg font-normal text-gray-500 lg:text-xl sm:px-16 xl:px-48 dark:text-gray-400">
+                    Manage your tasks, notes and contacts from a single point.</p>
+                <div
+                    class="flex flex-col mb-8 lg:mb-16 space-y-4 sm:flex-row sm:justify-center sm:space-y-0 sm:space-x-4">
+                    <InertiaLink href="/register" class="btn btn-primary">
+                        Signup
+                        <svg class="ml-2 -mr-1 w-5 h-5" fill="currentColor" viewBox="0 0 20 20"
+                            xmlns="http://www.w3.org/2000/svg">
+                            <path fill-rule="evenodd"
+                                d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z"
+                                clip-rule="evenodd"></path>
+                        </svg>
+                    </InertiaLink>
+                    <InertiaLink href="/login"
+                        class="btn btn-secondary gap-2">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none"
+                            stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+                            <path d="M20 21v-2a4 4 0 0 0-4-4H8a4 4 0 0 0-4 4v2"></path>
+                            <circle cx="12" cy="7" r="4"></circle>
+                        </svg>
+                        Login
+                    </InertiaLink>
+                </div>
+
+            </div>
+        </section>
+
+
+        <div class="px-4 mx-auto max-w-screen-xl lg:px-6 mt-6">
+            <div class="divider"></div>
+        </div>
+
+
+        <section class="bg-white dark:bg-gray-900">
+            <div
+                class="gap-8 items-center pt-10 md:pt-16 px-6 mx-auto max-w-screen-xl xl:gap-16 md:grid md:grid-cols-2 lg:px-6">
                 <img class="w-full dark:hidden" src="/images/cta-dashboard-mockup.svg" alt="dashboard image">
                 <img class="w-full hidden dark:block" src="/images/cta-dashboard-mockup-dark.svg" alt="dashboard image">
                 <div class="mt-4 md:mt-0">
                     <h2 class="mb-4 text-3xl font-extrabold text-gray-900 dark:text-white md:text-5xl lg:text-6xl"><span
-                            class="text-transparent bg-clip-text bg-gradient-to-r to-emerald-600 from-sky-400">Your
+                            class="text-transparent bg-clip-text bg-gradient-to-r to-red-600 from-sky-400">Your
                             Data<br /></span> Your Control.</h2>
-                    <p class="mb-6 font-light text-gray-500 md:text-lg dark:text-gray-400">A place for everything and everything in it's place.</p>
+                    <p class="mb-6 font-light text-gray-500 md:text-lg dark:text-gray-400">A place for everything and
+                        everything in it's place.</p>
                     <InertiaLink href="/register" class="btn btn-ghost text-white">
                         Get started
                         <svg class="ml-2 -mr-1 w-5 h-5" fill="currentColor" viewBox="0 0 20 20"
@@ -87,7 +140,8 @@ defineProps({
                             </svg>
                         </div>
                         <h3 class="mb-2 text-xl font-bold dark:text-white">Contacts</h3>
-                        <p class="text-gray-500 dark:text-gray-400">Easy access to your contacts and their information.</p>
+                        <p class="text-gray-500 dark:text-gray-400">Easy access to your contacts and their information.
+                        </p>
                     </div>
 
                 </div>
